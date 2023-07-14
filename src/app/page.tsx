0dddd1a658a184c060c9fcfdbdea7da375c77204
Chien-N.K <keaneahleo77@gmail.com>
2023-07-14 18:03:57 +0700

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,6 @@ export default function Home() {
           >
             By{" "}
             <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-            <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-            <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
           </a>
         </div>
       </div>
